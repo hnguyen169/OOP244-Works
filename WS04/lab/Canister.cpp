cdc754715e0f9d15600aba40a03edc36d53c5e8f
Harrison Nguyen <hnguyen169@myseneca.ca>
2024-06-05 21:18:21 -0400

    starting workshop 4

---
+++ WS04/lab/Canister.cpp
@@ -0,0 +1,16 @@
+//Name: Harrison Nguyen
+//Email: hnguyen169@myseneca.ca
+//ID: 167096239
+//Date Compeleted: 06/05/2024
+//I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <istream>
+#include <iomanip>
+#include <cstring>
+#include "Canister.h"
+using namespace std;
+namespace seneca {
+
+   
+}
