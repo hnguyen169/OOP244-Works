cd59148539187422682a19d5ca07988cc0357ea8
Harrison Nguyen <hnguyen169@myseneca.ca>
2024-06-15 18:50:48 -0400

    completed workshop 5

--- WS05/lab/Account.cpp
+++ WS05/lab/Account.cpp
@@ -1,7 +1,7 @@
 //Name: Harrison Nguyen
 //Email: hnguyen169@myseneca.ca
 //ID: 167096239
-//Date Compeleted: 06/13/2024
+//Date Compeleted: 06/15/2024
 //I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
 
 #define _CRT_SECURE_NO_WARNINGS
@@ -14,12 +14,11 @@ namespace seneca {
       m_number = -1;
       m_balance = 0.0;
    }
-   // New account
    Account::Account() {  
       m_number = 0;
       m_balance = 0.0;
    }
-   Account::Account(int number, double balance ) {
+   Account::Account(int number, double balance) {
       setEmpty();
       if (number >= 10000 && number <= 99999
          && balance > 0) {
@@ -27,7 +26,7 @@ namespace seneca {
          m_balance = balance;
       }
    }
-   std::ostream& Account::display() const{
+   std::ostream& Account::display() const {
       if (*this) {  
          cout << " ";
          cout << m_number;
@@ -48,5 +47,74 @@ namespace seneca {
       }
       return cout;
    }
-
+   Account::operator bool() const {
+       return m_number >= 10000 && m_number <= 99999;
+   }
+   Account::operator int() const {
+       return m_number;
+   }
+   Account::operator double() const {
+       return m_balance;
+   }
+   bool Account::operator~() const{
+       return m_number == 0;
+   }
+   Account& Account::operator=(int num) {
+       if (m_number == 0) {
+           if (num >= 10000 && num <= 99999) {
+               m_number = num;
+           }
+           else {
+               setEmpty();
+           }
+       }
+       return *this;
+   }
+   Account& Account::operator=(Account& acc) {
+       if (~(*this) && acc) {
+           m_number = acc.m_number;
+           m_balance = acc.m_balance;
+           acc.m_number = 0;
+           acc.m_balance = 0.0;
+       }
+       return *this;
+   }
+   Account& Account::operator+=(double val) {
+       if (*this && val > 0) {
+           m_balance += val;
+       }
+       return *this;
+   }
+   Account& Account::operator-=(double val) {
+       if (*this && val > 0 && m_balance >= val) {
+           m_balance -= val;
+       }
+       return *this;
+   }
+   Account& Account::operator<<(Account& acc) {
+       if (*this && acc && this != &acc) {
+           m_balance += acc.m_balance;
+           acc.m_balance = 0.0;
+       }
+       return *this;
+   }
+   Account& Account::operator>>(Account& acc) {
+       if (*this && acc && this != &acc) {
+           acc.m_balance += m_balance;
+           m_balance = 0.0;
+       }
+       return *this;
+   }
+   double operator+(const Account& acc1, const Account& acc2) {
+       if (acc1 && acc2) {
+           return acc1.operator double() + acc2.operator double();
+       }
+       return 0.0;
+   }
+   double operator+=(double& val, const Account& acc) {
+       if (acc) {
+           val += acc.operator double();
+       }
+       return val;
+   }
 }
--- WS05/lab/Account.h
+++ WS05/lab/Account.h
@@ -1,7 +1,7 @@
 //Name: Harrison Nguyen
 //Email: hnguyen169@myseneca.ca
 //ID: 167096239
-//Date Compeleted: 06/13/2024
+//Date Compeleted: 06/15/2024
 //I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
 
 #ifndef SENECA_ACCOUNT_H_
@@ -10,18 +10,51 @@
 
 namespace seneca {
    class Account {
-      int m_number;
-      double m_balance; 
+      int m_number; // account number
+      double m_balance; // account balance
+      // sets account to empty: m_number = -1, m_balance = 0.0
       void setEmpty();
    public:
+      // sets account to new: m_number = 0, m_balance = 0.0
       Account();
+      // sets account to incoming arguments if valid, otherwise sets account to empty
       Account(int number, double balance);
-      std::ostream& display()const;
-
-
-
+      // displays the account on screen
+      std::ostream& display() const;
+      // returns true if account is valid, false otherwise
+      operator bool() const;
+      // returns account number
+      operator int() const;
+      // returns balance value
+      operator double() const;
+      // returns true if account is new or not set, false otherwise
+      bool operator~() const;
+      // if new account, sets account number to the integer argument if valid.
+      // if integer is invalid, sets to invalid state.
+      // if not new account, takes no action.
+      Account& operator=(int num);
+      // if new account, sets account number and balance from the argument account if valid.
+      // then sets argument account to new state.
+      // if not new account or argument account is invalid, takes no action.
+      Account& operator=(Account& acc);
+      // if account is valid and val is positive, adds val to account balance otherwise, takes no action.
+      Account& operator+=(double val);
+      // if account is valid, val is positive, and there are enough funds then subtracts val from account balance.
+      // if account is invalid, value is negative, or not enough funds, takes no action.
+      Account& operator-=(double val);
+      // if both accounts are valid and not the same, transfers argument balance into current account balance.
+      // then sets balance of argument account to 0.0.
+      // if either accounts are invalid or the same account, takes no action.
+      Account& operator<<(Account& acc);
+      // if both accounts are valid and not the same, transfers current account balance into argument balance.
+      // then sets current account balance to 0.0.
+      // if either accounts are invalid or the same account, takes no action.
+      Account& operator>>(Account& acc);
    };
-   
-   
+   // if both accounts are valid, returns sum of balances of both accounts.
+   // if either account is invalid, returns zero.
+   double operator+(const Account& acc1, const Account& acc2);
+   // if account is valid, adds argument account balance to the val from argument then returns val.
+   double operator+=(double& val, const Account& acc);
 }
 #endif // SENECA_ACCOUNT_H_
