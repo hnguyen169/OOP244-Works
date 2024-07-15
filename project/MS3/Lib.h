1f885335ad10af15b1912ae05c6d6acb0ee722f0
Harrison Nguyen <hnguyen169@myseneca.ca>
2024-07-15 18:05:17 -0400

    starting ms3

---
+++ project/MS3/Lib.h
@@ -0,0 +1,13 @@
+/* Citation and Sources...
+Final Project Milestone 3
+Module: Lib
+Filename: Lib.h
+Version 1.0
+Author:	Harrison Nguyen
+Email: hnguyen169@myseneca.ca
+ID: 167096239
+Date Completed: 07/15/2024
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------*/
