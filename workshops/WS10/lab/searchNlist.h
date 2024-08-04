00eed01b0d77a43f0a12bbd9c48675192133087a
Harrison Nguyen <hnguyen169@myseneca.ca>
2024-08-03 20:23:19 -0400

    updated search function and added comments

--- workshops/WS10/lab/searchNlist.h
+++ workshops/WS10/lab/searchNlist.h
@@ -1,7 +1,7 @@
 //Name: Harrison Nguyen
 //Email: hnguyen169@myseneca.ca
 //ID: 167096239
-//Date Completed: 08/01/2024
+//Date Completed: 08/03/2024
 //I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
 
 #ifndef SEARCHNLIST_H
@@ -12,9 +12,23 @@
 
 namespace seneca {
 
-	// Function template for searching objects in an array and storing matches in a Collection
-	template <typename T>
-	bool search(Collection<T>& collection, const T* array, int arraySize, const typename T::KeyType& key) {
+	/*
+	 Function template for searching objects in an array and storing matches in a Collection.
+	 
+	 Template Type Requirements:
+	 - T must support the equality operator (operator==) with type K.
+	 - K can be any type that can be compared to type T using (operator==).
+	 - Collection<T> must have resize(int) to set collection size.
+	 - Collection<T> must have add(const T&) to add an element to the collection.
+
+	 @param collection: Reference to a Collection of templated objects to store the matches.
+	 @param array: Constant array of templated objects to search through.
+	 @param arraySize: Number of elements in the array.
+	 @param key: Key of templated type to search for in the array.
+	 @return true if at least one match is found, false otherwise.
+	 */
+	template <typename T, typename K>
+	bool search(Collection<T>& collection, const T* array, int arraySize, const K& key) {
 		bool found = false;
 		collection.resize(0);
 
@@ -27,7 +41,16 @@ namespace seneca {
 		return found;
 	};
 
-	// Function template for listing elements of an array
+	/*
+	 Function template for listing elements of an array
+	 
+	 Template Type Requirements:
+	 - T must support the insertion operator (operator<<) to be printed to an osteam.
+	 
+	 @param title: Constant char pointer for the title to be printed.
+	 @param array: Constant array of templated objects to be listed.
+	 @param arraySize: Number of elements in the array.
+	 */
 	template <typename T>
 	void listArrayElements(const char* title, const T* array, int arraySize) {
 		std::cout << title << std::endl;
