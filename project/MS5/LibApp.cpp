ca5331fb2ba5800b8609007a1df259af5e16562b
Harrison Nguyen <hnguyen169@myseneca.ca>
2024-08-04 21:59:35 -0400

    updated load, save, search functions. added getPub and new members

--- project/MS5/LibApp.cpp
+++ project/MS5/LibApp.cpp
@@ -12,8 +12,13 @@ I have done all the coding by myself and only copied the code
 that my professor provided to complete my workshops and assignments.
 -----------------------------------------------------------*/
 
+#define _CRT_SECURE_NO_WARNINGS
 #include "LibApp.h"
+#include "Book.h"
+#include "PublicationSelector.h"
 #include <iostream>
+#include <fstream>
+#include <cstring>
 
 using namespace std;
 
@@ -21,15 +26,22 @@ namespace seneca {
     LibApp::LibApp()
         : m_changed(false),
         m_mainMenu("Seneca Library Application"),
-        m_exitMenu("Changes have been made to the data, what would you like to do?")
+        m_exitMenu("Changes have been made to the data, what would you like to do?"),
+        m_pubTypeMenu("Choose the type of publication:"),
+        m_NOLP(0), m_LLRN(0)
     {
         m_mainMenu << "Add New Publication"
-            << "Remove Publication"
-            << "Checkout publication from library"
-            << "Return publication to library";
-
+                   << "Remove Publication"
+                   << "Checkout publication from library"
+                   << "Return publication to library";
+        
         m_exitMenu << "Save changes and exit"
-            << "Cancel and go back to the main menu";
+                   << "Cancel and go back to the main menu";
+
+        m_pubTypeMenu << "Book"
+                      << "Publication";
+
+        strcpy(m_filename, "LibRecs.txt");
 
         load();
     }
@@ -43,24 +55,102 @@ namespace seneca {
 
     void LibApp::load() {
         cout << "Loading Data\n";
+        ifstream file(m_filename);
+        char pubType{};
+
+        while (file >> pubType) {
+            file.ignore();
+            Publication* pub = nullptr;
+
+            if (pubType == 'B') {
+                pub = new Book;
+            }
+            else if (pubType == 'P') {
+                pub = new Publication;
+            }
+
+            if (pub) {
+                file >> *pub;
+                if (file) {
+                    m_PPA[m_NOLP++] = pub;
+                    m_LLRN = pub->getRef();
+                }
+                else {
+                    delete pub;
+                }
+            }
+        }
     }
 
     void LibApp::save() {
         cout << "Saving Data\n";
+        ofstream file(m_filename);
+
+        for (int i = 0; i < m_NOLP; ++i) {
+            if (m_PPA[i]->getRef() != 0) {
+                file << *m_PPA[i] << endl;
+            }
+        }
     }
 
-    void LibApp::search() {
-        cout << "Searching for publication\n";
+    void LibApp::search(bool all, bool onLoan) {
+        PublicationSelector selector("Select one of the following found matches:");
+        cout << "Choose the type of publication:\n";
+        int pubType = m_pubTypeMenu.run();
+
+        if (pubType == 0) {
+            cout << "Aborted!" << endl;
+            return;
+        }
+
+        char title[256];
+        cout << "Publication Title: ";
+        cin.ignore();
+        cin.getline(title, 256);
+
+        for (int i = 0; i < m_NOLP; ++i) {
+            if (m_PPA[i]->getRef() != 0) {
+                if (pubType == 1 && dynamic_cast<Book*>(m_PPA[i]) != nullptr && strstr(*m_PPA[i], title)) {
+                    if ((all) || (onLoan && m_PPA[i]->onLoan()) || (!onLoan && !m_PPA[i]->onLoan())) {
+                        selector << m_PPA[i];
+                    }
+                }
+                else if (pubType == 2 && dynamic_cast<Book*>(m_PPA[i]) == nullptr && strstr(*m_PPA[i], title)) {
+                    if ((all) || (onLoan && m_PPA[i]->onLoan()) || (!onLoan && !m_PPA[i]->onLoan())) {
+                        selector << m_PPA[i];
+                    }
+                }
+            }
+        }
+
+        if (selector) {
+            selector.sort();
+            int ref = selector.run();
+            if (ref != 0) {
+                cout << "Publication selected!" << endl;
+            }
+            else {
+                cout << "Aborted!" << endl;
+            }
+        }
+        else {
+            cout << "No matches found!" << endl;
+        }
     }
 
     void LibApp::returnPub() {
-        search();
+        search(true, true);
         cout << "Returning publication\n";
         cout << "Publication returned\n\n";
         m_changed = true;
     }
 
     void LibApp::newPublication() {
+        if (m_NOLP == SENECA_LIBRARY_CAPACITY) {
+            cout << "Library is at its maximum capacity!" << endl;
+            return;
+        }
+
         cout << "Adding new publication to library\n";
         if (confirm("Add this publication to library?")) {
             m_changed = true;
@@ -134,5 +224,14 @@ namespace seneca {
         }
         cout << "-------------------------------------------\n";
         cout << "Thanks for using Seneca Library Application" << endl;
+    }
+
+    Publication* LibApp::getPub(int libRef) {
+        for (int i = 0; i < m_NOLP; ++i) {
+            if (m_PPA[i]->getRef() == libRef) {
+                return m_PPA[i];
+            }
+        }
+        return nullptr;
     }
 }
--- project/MS5/LibApp.h
+++ project/MS5/LibApp.h
@@ -16,16 +16,25 @@ that my professor provided to complete my workshops and assignments.
 #define SENECA_LIBAPP_H
 
 #include "Menu.h"
+#include "Lib.h"
+#include "Publication.h"
 
 namespace seneca {
     class LibApp {
+        char m_filename[256];
+        Publication* m_PPA[SENECA_LIBRARY_CAPACITY];
+        int m_NOLP;
+        int m_LLRN;
+        Menu m_pubTypeMenu;
+        Publication* getPub(int libRef);
+
         bool m_changed;
         Menu m_mainMenu;
         Menu m_exitMenu;
         bool confirm(const char* message);
         void load();
         void save();
-        void search();
+        void search(bool all = true, bool onLoan = false);
         void returnPub();
         void newPublication();
         void removePublication();
